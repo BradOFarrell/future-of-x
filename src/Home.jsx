0087afed9da5b0985bcd9a68b771408ca061fb06
BradOFarrell <bradofarrell@gmail.com>
2018-08-26 18:30:50 -0400

    Validate checkboxes

--- src/Home.jsx
+++ src/Home.jsx
@@ -13,27 +13,29 @@ class Home extends React.Component {
       <br/>
 
       <div className="maintext">
-      <p>This web-app will help you develop a unique perspective on the future of an industry, a company, or even your own personal future. For example, you could use it forecast The Future of Cellphones, The Future of Baseball, or The Future of San Francisco.</p> 
-      <p>Neuroscientists have discovered techniques for increasing our cognitive flexibility, which allows us to imagine new possibilities. These scientific techniques form the basis of Futures Thinking.</p>
+        <p>This web-app will help you develop a unique perspective on the future of an industry, a company, or even your own personal future. For example, you could use it forecast The Future of Cellphones, The Future of Baseball, or The Future of San Francisco.</p> 
+        <p>Neuroscientists have discovered techniques for increasing our cognitive flexibility, which allows us to imagine new possibilities. These scientific techniques form the basis of Futures Thinking.</p>
       </div>
       <div className="reminderbox">
-      <span className="remindertitle">What is Futures Thinking?</span>
-      <br/>
-      Every successful strategy is based on a great insight about the future. By systematically studying the long-term future you can make better decisions in the present. Futures Thinking provides the foresight that sparks critical new insights ultimately leading to better actions today.
+        <span className="remindertitle">What is Futures Thinking?</span>
+        <br/>
+        Every successful strategy is based on a great insight about the future. By systematically studying the long-term future you can make better decisions in the present. Futures Thinking provides the foresight that sparks critical new insights ultimately leading to better actions today.
       </div>
       <br/>  
       <div className="maintext">
-      <p>This application is a collection of Futures Thinking tools, adapted from the <a href="http://www.iftf.org/futuresthinking/">IFTF Futures Thinking Toolkit</a>. As you progress through each tool, you'll be building a complex model for the future that will be displayed on the final step.</p>    
+        <p>This application is a collection of Futures Thinking tools, adapted from the <a href="http://www.iftf.org/futuresthinking/">IFTF Futures Thinking Toolkit</a>. As you progress through each tool, you'll be building a complex model for the future that will be displayed on the final step.</p>    
       </div>
-      <br/>    
-      <div className="responsebox">
-      <p className="btn btn-primary"  onClick={() => {window.location = "/one"}}>Get Started</p> 
-      <br/>
-    </div>
+      <br/> 
     </div>
   </div>
+
+  <div className="responsebox">
+  <span className="warnerror" id="warn"></span>
   <br/>
-  <button type="button" class="btn btn-secondary">Go back</button>     
+  <p className="btn btn-primary" onClick={() => {window.location = "/one"}}>Start</p>
+  <br/>
+  </div>
+
 </div>  
       );        
     }
--- src/StepOne.jsx
+++ src/StepOne.jsx
@@ -3,9 +3,7 @@ import Parser from 'rss-parser';
 import './App.css';
 
 const parser = new Parser();
-let searchTerm = "";
 const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
-
 (async () => {
  
   const feed = await parser.parseURL(CORS_PROXY + 'https://news.google.com/news/rss/search/section/q/fishing');
@@ -16,30 +14,65 @@ class StepOne extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      newsfeed: new Array()
+      newsfeed: new Array(),
+      searchTerm: "",
+      articles: new Array(),
+      checkedBoxes: new Array(),
+      totalCheckedBoxes: 0
+    }
+  }
+  checkboxChange = (e) => {
+    console.log(e.target.id)
+    console.log(e.target.checked)  
+    if(e.target.checked){
+      this.state.checkedBoxes.push(e.target.id)
+    } else {
+      for(let i = 0; i < this.state.checkedBoxes.length; i++){
+        if(this.state.checkedBoxes[i] == e.target.id)
+        this.state.checkedBoxes.splice(i, 1);
+      }
+    }
+    if(this.state.checkedBoxes.length == 3){
+      document.getElementById("warn").innerHTML = "";
+      for(let i = 0; i < this.state.totalCheckedBoxes; i++){
+        const boxId = "checkbox"+i;
+        if(boxId != this.state.checkedBoxes[0] && boxId != this.state.checkedBoxes[1] && boxId != this.state.checkedBoxes[2]){
+          document.getElementById("checkboxContainer"+i).style.display = "none";
+        }
+      }
+    } else {
+      for(let i = 0; i < this.state.totalCheckedBoxes; i++){
+        document.getElementById("checkboxContainer"+i).style.display = "table";
+      }
     }
   }
   feedlist() {
     let output = {};
-//    output = (<li>{this.state.newsfeed[0].title}</li>);
+    let id = 0;
+    console.log("feedlist");
     if(this.state.newsfeed[0]){
       output = this.state.newsfeed.map(e =>{
-        return (<tr><td><input type="checkbox"></input></td><td key={e.title}><a href={e.link} target="_blank">{e.title}</a></td></tr>);
-      });  
+        const output = (<tr id={"checkboxContainer"+id}><td>
+        <input type="checkbox" id={"checkbox"+id} onChange={this.checkboxChange}></input></td>
+        <td key={e.title}><a href={e.link} target="_blank">{e.title}</a></td></tr>)
+        id++;
+        return (output);
+      });
+      this.state.totalCheckedBoxes = id;
       return (<table className="table table-striped table-dark">{output}</table>)
     } else {
       return (<p><br/>Use the search box above to generate headlines.</p>)
-    }
+    }  
   }
-  handleChange = (e) => {
-    searchTerm = e.target.value;
-    console.log(searchTerm)
+  searchChange = (e) => {
+    this.state.searchTerm = e.target.value;
+    console.log(this.state.searchTerm)
   }
   handleSubmit= (e) => {
     e.preventDefault();
     const output = new Array();
     (async () => {
-      const feed = await parser.parseURL(CORS_PROXY + 'https://news.google.com/news/rss/search/section/q/' + searchTerm);
+      const feed = await parser.parseURL(CORS_PROXY + 'https://news.google.com/news/rss/search/section/q/' + this.state.searchTerm);
 
       feed.items.forEach(item => {
        output.push({title: item.title, link: item.link})
@@ -48,51 +81,62 @@ class StepOne extends React.Component{
       this.setState({newsfeed: output})
     })();
   }
+  validateNext = () => {
+    if(this.state.checkedBoxes.length == 3){
+      // Send data to app state
+      window.location = "/two";
+    } else {
+      document.getElementById("warn").innerHTML = "You must select three headlines.";
+    }
+  }
   render() {
     {
       return (
-        <div className="App">
-  
-    <div className="jumbotron jumbotron-fluid">
+<div className="App">
+  <div className="jumbotron jumbotron-fluid">
     <div className="container">
       <br/>
-      <h2 id="title">Step 1: Look Back To Look Forward</h2>
+      <h2 id="title">Step 1: Collect Signals</h2>
       <br/>
-  
+
       <div className="container-fluid maintext hide">
-      <p>
+        <p>
         Imagining a future that is very different from today can often be challenging. But change happens – and it can happen faster than we expect.
         <br/><br/>
-         Looking to the past helps us plan for the future. Use ths tool to search for recent headlines. Choose three that you think signal a dramatic change in the status quo for [X]. We will use them on a later step.</p>    
+        We will use this tool to look for headlines that seem to signal a change in the future. A <em>signal</em> is a recent small or local innovation with the potential to scale in impact and affect other places, people, or markets.
+        <br/><br/>
+        </p>    
       </div>
-    
+
       <form onSubmit={this.handleSubmit}>
-      <input type="text" placeholder="SEARCH TERM" className="inputText" id="myUnit"
-        onChange={this.handleChange}/>
-      <button type="submit" className="btn btn-primary">GO</button>
+        <input type="text" placeholder="SEARCH TERM" className="inputText" id="myUnit"
+        onChange={this.searchChange}/>
+        <button type="submit" className="btn btn-primary">GO</button>
       </form>
 
       <div className="reminderbox">
-      {this.feedlist()}  
+        {this.feedlist()}  
       </div>
-  
+
       <br/>
-  
-      <div className="maintext">
-      <p>Select three headlines to move on to the next step.
-      </p>    
+
+      <div className="container-fluid maintext hide">
+        <p>
+          Select three headlines that seem to signal <em>different</em> directions for the future. 
+          We will use your selection again on a later step.
+          <br/><br/>
+        </p>    
       </div>
-  
-      <div className="responsebox">
-      <p className="btn btn-primary"  onClick={() => {window.location = "/Two"}}>Next Step</p> 
-      </div>
-  
     </div>
   </div>
+
+  <div className="responsebox">
+  <span className="warnerror" id="warn"></span>
   <br/>
-  <p className="btn btn-secondary">Go back</p>     
-        </div>
-  
+  <p className="btn btn-primary"  onClick={this.validateNext}>Next Step</p>
+  <br/>
+  </div>
+</div>
       );        
     }
   }
